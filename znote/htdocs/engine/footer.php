39925e90d0c67e3b82d6c2692325e2a9e767a54f
Marcin <mgoral1@hotmail.com>
2025-12-18 02:07:17 +0100

    Clean up the project

--- znote/htdocs/engine/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-	&copy; Znote AAC.
-	<?php
-	$finish = microtime(true);
-	$total_time = round(($finish - $start), 4);
-	echo 'Server date and clock is: '. getClock(false, true) .' Page generated in '. $total_time .' seconds. ';
-	?>
-</footer>
